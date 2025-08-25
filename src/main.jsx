a3fe19a7350bb80450b7fad034e5eafba528bf90
robertolima84 <betinho_cfl@hotmail.com>
2025-08-25 00:56:42 -0300

    criando a rota de navegação

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { GlobalStyles } from "./styles/GlobalStyles.js"; // importa estilos globais.
+import { RouterProvider } from "react-router-dom";
+import router from './routes.jsx'
 
-import Home from "./pages/Home"; // importa do modo padrão apenas 1 componente por arquivo " modo default".
+
+//import Home from "./pages/Home"; // importa do modo padrão apenas 1 componente por arquivo " modo default".
 
 // import {App, App2} from './App.jsx' // importa mais de 1 componente por arquivo " modo nomeado".
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <GlobalStyles />
-    <Home />
-    <GlobalStyles />
+    <RouterProvider router={router} />
+  
   </StrictMode>
 );
--- src/pages/Home/ListUsers/index.jsx
+++ src/pages/Home/ListUsers/index.jsx
@@ -6,7 +6,17 @@ function ListUsers() {
 
 
 
+   return(
+
+    <div>
+        <h1>Listagem de Usuários</h1>
+
+    </div>
+
+   )
 
 
-    
 }
+
+
+export default ListUsers
